76f464ccf77c62e4c67081a74f2bd73228928da4
amine <root@localhost.localdomain>
2020-06-20 21:44:15 +0200

    fixing typo

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,6 +2,6 @@
 set -e
 bash -c "echo \"${DOCKER_PASSWORD}\" | docker login --username \"${DOCKER_LOGIN}\" --password-stdin"
 
-docker tag zutherb/monolithic-shop:latest Amine710-info/monolithic_shop:latest
+docker tag zutherb/monolithic-shop:latest amine710-info/monolithic_shop:latest
 docker push $DOCKER_LOGIN/monolithic-shop:latest
 echo "ok"
