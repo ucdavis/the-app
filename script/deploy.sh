b98d76aa7d3b6800b17a68c0dc86a439e1fa0469
amine <root@localhost.localdomain>
2020-06-20 21:34:14 +0200

    final step in projet

---
+++ script/deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e
+bash -c "echo \"${DOCKER_PASSWORD}\" | docker login --username \"${DOCKER_LOGIN}\" --password-stdin"
+
+docker tag zutherb/monolithic-shop:latest Amine710-info/monolithic_shop:latest
+docker push $DOCKER_LOGIN/monolithic-shop:latest
+echo "ok"
