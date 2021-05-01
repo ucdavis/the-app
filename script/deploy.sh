af50fd14f62ed31a759762a4bbad59ed5f805695
MarceloR <marcelo.rui11@gmail.com>
2021-05-01 17:36:29 -0500

    testing1

---
+++ script/deploy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+
+echo "${DOCKER_PASSWORD}" | docker login --username \"${DOCKER_LOGIN}\" --password-stdin
+docker tag ucdavis/web1066-the-app:latest $DOCKER_LOGIN/web1066-the-app:latest
+docker push $DOCKER_LOGIN/web1066-the-app:latest
