34045c5216310cbc6b6948dac49dc63ac16efc01
MarceloR <marcelo.rui11@gmail.com>
2021-05-01 18:32:35 -0500

    testing tag

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,5 +2,5 @@
 set -e
 
 echo "${DOCKER_PASSWORD}" | docker login --username \"${DOCKER_LOGIN}\" --password-stdin
-docker tag zutherb/monolithic-shop:latest $DOCKER_LOGIN/web1066-the-app:latest
+docker tag ucdavis/web1066-the-app:latest $DOCKER_LOGIN/web1066-the-app:latest
 docker push $DOCKER_LOGIN/web1066-the-app:latest
