8d5e03f8123cc117250c7eca0e7a92436e4d485c
EmyRukundo <39124680+EmyRukundo@users.noreply.github.com>
2021-07-26 12:12:43 +0200

    added docker deploy

---
+++ script/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+bash -c "echo \"${DOCKER_PASS}\" | docker login --username \"${DOCKER_LOGIN}\" --password-stdin"
+docker pull zutherb/monolithic-shop:latest
+docker tag zutherb/monolithic-shop:latest ${DOCKER_LOGIN}/monolithic-shop:latest
+docker push ${DOCKER_LOGIN}/monolithic-shop:latest
